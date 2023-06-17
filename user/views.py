53625c70ba5fe084cfaeef59bcb6c52a3de9b827
GeorgePavlej <georgepavlej@gmail.com>
2023-06-17 18:09:18 +0100

    Add user logout

--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 
@@ -17,7 +19,18 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+        response = Response({"message": "Logged out successfully"})
+        response.delete_cookie("refresh_token")
+
+        return response
