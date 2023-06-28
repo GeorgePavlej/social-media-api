85770b32f06803f0856f7515af3f2e878ba887e2
GeorgePavlej <georgepavlej@gmail.com>
2023-06-28 19:20:34 +0100

    Add FollowUserViewSet

--- user/views.py
+++ user/views.py
@@ -1,19 +1,21 @@
 from typing import Any
 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, viewsets, filters, permissions
+from rest_framework import generics, viewsets, filters, permissions, status
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.models import Profile
+from user.models import Profile, User, Follow
 from user.permissions import IsOwnerOrReadOnly
 from user.serializers import (
     UserSerializer,
     AuthTokenSerializer,
-    ProfileSerializer
+    ProfileSerializer,
+    FollowSerializer,
 )
 
 
@@ -50,4 +52,23 @@ class ProfileUserViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ["bio", "name"]
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class FollowUserViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        followed_user = User.objects.get(id=serializer.validated_data["followed"].id)
+
+        if self.request.user == followed_user:
+            raise ValidationError({"message": "You cannot follow yourself."})
+
+        if Follow.objects.filter(
+            follower=self.request.user, followed=followed_user
+        ).exists():
+            raise ValidationError({"message": "You are already following this user."})
+
+        serializer.save(follower=self.request.user)
