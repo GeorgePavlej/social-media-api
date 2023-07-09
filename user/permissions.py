abee9d460b4287b5e169fdc5ca8d47e828399b9b
GeorgePavlej <georgepavlej@gmail.com>
2023-07-09 18:33:38 +0100

    Formatted code in permissions.py

--- user/permissions.py
+++ user/permissions.py
@@ -5,6 +5,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of a profile to edit it.
     """
+
     def has_object_permission(self, request, view, obj) -> bool:
         if request.method in permissions.SAFE_METHODS:
             return True
